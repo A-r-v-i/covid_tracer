6ca4a72bdf9d7b49e47b494c12708b1368ab57fa
A-r-v-i <babuaravindsv@gmail.com>
2020-04-06 10:51:51 +0530

    Created the project and starts working on it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { fetchData } from './components/util/data';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const country = 'India';
+
+export default class App extends Component {
+
+  async componentDidMount() {
+    const data = await fetchData();
+    const countryStatus = "data.data.countryDetail"+country; 
+    console.log(data.data.countryDetail);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Its corona tym.....</h1>
+      </div>
+    )
+  }
 }
-
-export default App;
